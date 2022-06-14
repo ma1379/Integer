da283bb50d8c5e902d02ab8be3481f7de24ffc15
Puj Manohar <23manohp@elmbrookstudents.org>
2022-06-14 13:04:49 -0500

    Added Print

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Please enter a series of integers separated by spaces.");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter a series of integers separated by spaces. Do not end with a space.");
 String stringOfNumbers = Console.ReadLine();
 String[] numbersSplit = stringOfNumbers.Split(' ');
 //Converting to an int array
 int[] values = new int[numbersSplit.Length];
 for (int i = 0; i < numbersSplit.Length; i++) {
 values[i] = Int32.Parse(numbersSplit[i]);
+}
+
+for ( int i = 0 ; i < values.Length ; i++ ){
+Console.WriteLine( values[i]);
 }
